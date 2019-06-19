82d8cc08b6b75dd3e4da04ea1e164cfa7dac1d87
losevfa <losevfa2711@gmail.com>
2019-06-19 11:00:42 +0700

    new folders
    added folders and change  view name

---
+++ FLChat/FLChat/Controllers/LogInVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FLChat
+//
+//  Created by Softomate on 6/19/19.
+//  Copyright © 2019 losev.feder2711@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
