094ac87f4be4824fee25ed94a59f66d82699cfa5
fedor <losevfa2711@gmail.com>
2019-06-22 14:27:49 +0700

    Protocol ShowHideKeyboard

---
+++ FLChat/FLChat/Resources/ShowHideKeyboard.swift
@@ -0,0 +1,15 @@
+//
+//  ShowHideKeyboard.swift
+//  FLChat
+//
+//  Created by Fedor Losev on 22/06/2019.
+//  Copyright © 2019 losev.feder2711@gmail.com. All rights reserved.
+//
+
+import Foundation
+
+protocol ShowHideKeyboard {
+    func removeObserverKeyboard()
+    func addObserverKeyboard()
+    func keyboardWillChanged(notification: Notification)
+}
