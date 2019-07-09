f393688fea406c98f25ce36c5a68274696f7eb80
fedor <losevfa2711@gmail.com>
2019-07-09 20:17:28 +0700

    added transition for settins view

---
+++ FLChat/FLChat/Views/SlideInTransiotion.swift
@@ -0,0 +1,49 @@
+ //
+//  SlideInTransiotion.swift
+//  FLChat
+//
+//  Created by Fedor Losev on 09/07/2019.
+//  Copyright © 2019 losev.feder2711@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class SlideInTransiotion: NSObject, UIViewControllerAnimatedTransitioning {
+
+    var isPresenting = false
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let toViewController = transitionContext.viewController(forKey: .to) else { return }
+        guard let fromViewController = transitionContext.viewController(forKey: .from) else { return }
+        
+        let containerView = transitionContext.containerView
+        
+        let finalWidth = toViewController.view.bounds.width * 0.77
+        let finalHeight = toViewController.view.bounds.height
+        
+        if isPresenting{
+            containerView.addSubview(toViewController.view)
+            toViewController.view.frame = CGRect(x: -finalWidth, y: 0, width: finalWidth, height: finalHeight)
+        }
+        
+        let transform = {
+            toViewController.view.transform = CGAffineTransform(translationX: finalWidth, y: 0)
+        }
+        
+        let identity = {
+            fromViewController.view.transform = .identity
+        }
+        
+        let duration = transitionDuration(using: transitionContext)
+        let isCancelled = transitionContext.transitionWasCancelled
+        UIView.animate(withDuration: duration, animations: {
+            self.isPresenting ? transform() : identity()
+        }) { (_) in
+            transitionContext.completeTransition(!isCancelled)
+        }
+    }
+}
