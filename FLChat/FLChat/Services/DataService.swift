69e2f852ce82ab1355662c504398bf2803cbd6aa
losevfa <losevfa2711@gmail.com>
2019-07-15 11:32:05 +0700

    added FirebaseDatabase to  project

---
+++ FLChat/FLChat/Services/DataService.swift
@@ -0,0 +1,20 @@
+//
+//  DataService.swift
+//  FLChat
+//
+//  Created by Softomate on 7/15/19.
+//  Copyright © 2019 losev.feder2711@gmail.com. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    
+    let sharedInstance = DataService()
+    
+    
+
+}
