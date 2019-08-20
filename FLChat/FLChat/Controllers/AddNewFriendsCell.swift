26f8d95b6384f53b8522014ed870dd0c27f46fe1
fedor <losevfa2711@gmail.com>
2019-08-20 18:38:06 +0700

    created viewController

---
+++ FLChat/FLChat/Controllers/AddNewFriendsCell.swift
@@ -0,0 +1,13 @@
+//
+//  AddNewFriendsCell.swift
+//  FLChat
+//
+//  Created by Fedor Losev on 20/08/2019.
+//  Copyright © 2019 losev.feder2711@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class AddNewFriendsCell: UICollectionViewCell {
+    
+}
---
+++ FLChat/FLChat/Controllers/AddNewFriendsVC.swift
@@ -0,0 +1,42 @@
+//
+//  AddNewFriendsVC.swift
+//  FLChat
+//
+//  Created by Fedor Losev on 20/08/2019.
+//  Copyright © 2019 losev.feder2711@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class AddNewFriendsVC: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension AddNewFriendsVC: UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
