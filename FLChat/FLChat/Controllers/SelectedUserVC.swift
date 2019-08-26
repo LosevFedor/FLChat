6e9c592f04bfd3f01a939a33f0de800986fed8fc
fedor <losevfa2711@gmail.com>
2019-08-26 18:20:08 +0700

    send request to friend part 1

--- FLChat/FLChat/Controllers/SelectedUserVC.swift
+++ FLChat/FLChat/Controllers/SelectedUserVC.swift
@@ -7,11 +7,10 @@
 //
 
 import UIKit
+import Firebase
 
 class SelectedUserVC: UIViewController {
 
-    
-    
     @IBOutlet weak var selectedUserNameLabel: UILabel!
     @IBOutlet weak var selectedUserEmailLabel: UILabel!
     @IBOutlet weak var selectedUserStatusLabel: UILabel!
@@ -24,6 +23,7 @@ class SelectedUserVC: UIViewController {
     @IBOutlet weak var doSomthingButton: UIButton!
     
     var currentBackgroundUserImg:UIImage!
+    var currentUserId: String!
     var currentUserName:String!
     var currentUserImage: UIImage!
     var currentUserEmail:String!
@@ -44,8 +44,8 @@ class SelectedUserVC: UIViewController {
         super.viewDidLoad()
         selectedUserNameLabel.text = currentUserName
         selectedUserImg.image = currentUserImage
-        selectedUserEmailLabel.text = currentUserEmail
-        selectedUserPhoneLabel.text = currentUserPhone
+        selectedUserEmailLabel.text = "Email: \(currentUserEmail!)"
+        selectedUserPhoneLabel.text = "Phone: \(currentUserPhone!)"
         selectedUserStatusLabel.text = currentUserStatus
         selectedUserWhatYouCanToDoLabel.text = currentUserWhatYouCanToDo
         selectedBackgroundUserImg.image = currentBackgroundUserImg
@@ -53,12 +53,13 @@ class SelectedUserVC: UIViewController {
         addBlureEffeckForselectedBackgroundUserUmg()
     }
     
-    func initData(_ name: String, _ image: UIImage, _ email: String, _ phone: String, _ status: Bool){
+    func initData(_ id: String, _ name: String, _ image: UIImage, _ email: String, _ phone: String, _ status: Bool){
+        currentUserId = id
         currentUserName = name
         currentUserImage = image
         currentBackgroundUserImg = image
-        currentUserEmail = "Email: \(email)"
-        currentUserPhone = "Phone: \(phone)"
+        currentUserEmail = email
+        currentUserPhone = phone
         currentUserStatus = { () -> String in
             if status{
                 return "online"
@@ -66,16 +67,58 @@ class SelectedUserVC: UIViewController {
                 return "ofline"
             }
         }()
-        currentUserWhatYouCanToDo = "You can sand message this is user"
+        currentUserWhatYouCanToDo = "You don't have this is person in to your friends"
     }
     
-    @IBAction func BackBtnPressed(_ sender: Any) {
+    @IBAction func backBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func doSomthingButton(_ sender: Any){
+    @IBAction func sendFrienRequestBtnPressed(_ sender: Any){
         
+        let toId = currentUserId!
+        let fromId = DataService.instance.REF_UID
+        let timeStamp = Double(NSDate().timeIntervalSince1970)
+        let msgRequestFriend = "Hi! I want to be your friend"
+        
+        let toUserParams = toUserFriendRequest()
+        let fromUserParams = fromUserFriendRequest()
+        
+        DataService.instance.createRequestAddFriendIntoDB(fromId, toId, timeStamp, msgRequestFriend, fromUserParams, toUserParams)
     }
     
+    private func toUserFriendRequest() -> Dictionary<String,Any>{
+        let name = currentUserName!
+        
+        let image = getImageURL(currentUserImage)
+        let email = currentUserEmail!
+        let status = currentUserStatus!
+        
+        let userFields: Dictionary<String,Any> = ["toUserName": name, "toUserImage": image, "toUserEmail": email, "toUserStatus":status]
+        return userFields
+    }
+    
+    private func fromUserFriendRequest() -> Dictionary<String,Any>{
+        let name = User.instance.name
+        let image = User.instance.image
+        let email = User.instance.email
+        let status = User.instance.statusOnline
+        
+        let whoSendRequest: Dictionary<String,Any> = ["fromUserName": name, "fromUserImage": image, "fromUserEmail": email, "fromUserStatus": status]
+        return whoSendRequest
+    }
+    
+    private func getImageURL(_ image: UIImage) -> String{
+        let ref = DataService.instance.REF_STORAGE_BASE.child(currentUserId)
+        var convertedImageString = ""
+        DispatchQueue.main.async {
+            ref.downloadURL { (url, error) in
+                guard let url = url else { return }
+                convertedImageString = url.absoluteString
+            }
+        }
+        
+        return convertedImageString
+    }
 
 }
